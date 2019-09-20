a3e125199eadf426298ee2d776006073bd9d82f0
dorota-nowicz <55583018+dorota-nowicz@users.noreply.github.com>
2019-09-20 12:10:08 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET upload page. */
+router.get('/uploadfile', function(req, res, next) {
+	 res.render('uploadfile', { title: 'Upload File Test' });
+
+});
+
+/* POST file. */
+router.post('/uploadfile', function(req, res, next) {
+	 var form = new formidable.IncomingForm();
+	
+	 form.parse(req, function (err, fields, files) {
+		 var oldpath = files.filetoupload.path;
+		 var newpath = 'C:/0938_EMUASA/' + files.filetoupload.name;
+		 fs.rename(oldpath, newpath, function (err) {
+			if (err) throw err;
+			res.render('uploadfile', { title: 'Upload File Test' , msg: 'File uploaded and moved!'})
+		 });
+	 });
+					
+});
+
+
+module.exports = router;
